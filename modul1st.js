cc49fc74a1bf6ca382fdc9bf1373e437c6245a91
vaibhav <vpatil7382@gmail.com>
2023-04-27 18:09:49 +0530

    module completed

---
+++ modul1st.js
@@ -0,0 +1,6 @@
+// const simple2 = require("./module2nd");
+
+// import simple2 from "./module2nd.mjs"
+import * as a2 from "./module2nd.mjs"
+console.log(a2.simple())
+// simple2();
---
+++ module2nd.mjs
@@ -0,0 +1,22 @@
+ export function simple() {
+  console.log("Vaibhav is the back");
+  return 45
+}
+
+
+export default function simple2() {
+  console.log("Vaibhav is king");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// module.exports = simple;
