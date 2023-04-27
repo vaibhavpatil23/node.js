e64bd5b1b11925a664eea8bdd981e3c0b4148055
vaibhav <vpatil7382@gmail.com>
2023-04-27 17:43:57 +0530

    node sync procces

--- fsmodule.js
+++ fsmodule.js
@@ -1,5 +1,16 @@
 const fs = require("fs");
-fs.readFile("file.txt", "utf8", (err, data) => {
-  console.log(err, data);
-});
+// fs.readFile("file.txt", "utf8", (err, data) => {
+//   console.log(err, data);
+// });
+
+// const a = fs.readFileSync('file.txt')
+// console.log(a.toString())
+
+// fs.writeFile('file2.txt','This is the data',()=>{
+//   console.log("Written to the file")
+// })
+
+b = fs.writeFileSync("file2.txt", "This is the data2");
+console.log(b);
+
 console.log("finished reading file");
