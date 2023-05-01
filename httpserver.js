395e13186a92d7c50261baab27e82f979a1bec61
vaibhav <vpatil7382@gmail.com>
2023-05-01 17:53:45 +0530

    blocking and non blocking nodejs code

--- httpserver.js
+++ httpserver.js
@@ -4,12 +4,14 @@ const http = require("http");
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-    console.log(req.url)
+  console.log(req.url); 
   res.statusCode = 200;
-  res.setHeader('Content-Type','text/html')
-  res.end('<h1>This is the vaibhav patil is so cute person and </h1> <p> I am the best</p>')
+  res.setHeader("Content-Type", "text/html");
+  res.end(
+    "<h1>This is the vaibhav patil is so cute person and </h1> <p> I am the best</p>"
+  );
 });
 
-server.listen(port,() =>{
-  console.log  (`server is listening on port ${port}`)
-})
+server.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
+});
