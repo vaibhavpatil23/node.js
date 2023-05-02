94966864d5741be2dbff9a43c63edd08532d7517
vaibhav <vpatil7382@gmail.com>
2023-05-02 19:11:11 +0530

    complet app js folder

--- Expressapp/app.js
+++ Expressapp/app.js
@@ -1,25 +1,41 @@
+
 const express = require("express");
+const path = require("path");
+const fs = require("fs");
 const app = express();
 const port = 80;
-app.use("/static", express.static("static"));
 
-app.set("view engine", "pug");
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+app.use(express.urlencoded())
 
-app.set("views", path.join(__dirname, "views"));
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+ 
+// ENDPOINTS
+app.get('/', (req, res)=>{
+    const con = "This is the best content on the internet so far so use it wisely"
+    const params = {'title': 'PubG is the best game', "content": con}
+    res.status(200).render('index.pug', params);
+})
 
-app.get("/", (req, res) => {
-    res.static(200).render('demo',{title:'Hey brothher',massege: "Hello there"});
-  });
+app.post('/', (req, res)=>{
+    name = req.body.name
+    age = req.body.age
+    gender = req.body.gender
+    address = req.body.address
+    more = req.body.more
 
-app.get("/", (req, res) => {
-  res.static(200).send("This is homepage of my first express app");
-});
-app.get("/about", (req, res) => {
-  res.static(200).send("This is about page of my first express app");
-});
-app.get("/this", (req, res) => {
-  res.static(200).send("This is contact page of my first express app");
-});
-app.get("/about", (req, res) => {
-  res.static(200).send("This is serverpage of my first express app");
+    let outputToWrite = `the name of the client is ${name}, ${age} years old, ${gender}, residing at ${address}. More about him/her: ${more}`
+    fs.writeFileSync('output.txt', outputToWrite)
+    const params = {'message': 'Your form has been submitted successfully'}
+    res.status(200).render('index.pug', params);
+
+})
+
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
 });
